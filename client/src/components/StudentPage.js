c06ed97e487154f2ec645854203a7cfa4303549d
Monica Garcia <monicairenegarcia@gmail.com>
2024-06-25 13:48:37 -0700

    students GET by id front and back end functional

--- client/src/components/StudentPage.js
+++ client/src/components/StudentPage.js
@@ -1,7 +1,47 @@
 import React, { useEffect, useState } from "react";
+import { useHistory, useLocation } from 'react-router-dom'
 
 function StudentPage() {
-    return (<></>)
+    const [studentToDisplay, setStudentToDisplay] = useState({})
+    
+    const location = useLocation()
+    const url = location.pathname
+    const parts = url.split("/")
+    const id = parts[2]
+    
+    useEffect(() => {
+        fetch(`http://127.0.0.1:5555/students/${id}`, {
+        method: "GET",
+        })
+        .then(res => {
+            if (res.ok) {
+                res.json().then(data => setStudentToDisplay(data))
+            }
+            else {
+                console.log("error: " + res)
+            }
+        })
+        .catch(error => {
+            console.error("Error parsing JSON:", error);
+        })
+    }, [])
+    return (
+        <div>
+        {studentToDisplay ? (
+            <>
+                <h1>{studentToDisplay.first_name} {studentToDisplay.last_name}</h1>
+                <h2>Grade: {studentToDisplay.grade}</h2>
+                <h2>Currently enrolled in the following courses:</h2>
+                <h3>(Click on the course for further course details)</h3>
+                {studentToDisplay.courses ? studentToDisplay.courses.map((course) => {
+                    return <div key={course.id}>
+                        <a href={`/courses/${course.id}`}>{course.name}</a>
+                        </div>
+                }) : (<h3>No courses to display</h3>)}
+            </>
+        ) : (<h1>Loading...</h1>)}
+    </div>
+    )
 }
 
 export default StudentPage
